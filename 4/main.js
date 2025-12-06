ac83e2e72c5ba9c5db15fdfda4ec5b101052e43e
Robert Lorentz <robert.lorentz95@gmail.com>
2025-12-06 11:17:00 +0100

    4 part one

---
+++ 4/main.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const { readLines } = require('../helper/helper');
+
+const inputPath = path.join(__dirname, 'input', 'input.txt');
+const input = readLines(inputPath);
+
+paper = '@';
+maxPaper = 4;
+output = 0;
+
+class Grid {
+    constructor(grid) {
+        this.grid = grid;
+    }
+
+    get() {
+        return this.grid;
+    }
+
+    adjacent(x, y) {
+        var adjacents = [];
+
+        if (x > 0) {
+            if (y > 0) {
+                adjacents.push(grid[x-1][y-1]);
+                
+            }
+            if (y < grid[x].length - 1) {
+                adjacents.push(grid[x-1][y+1]);
+            }
+            adjacents.push(grid[x-1][y]);
+        }
+
+        if (y > 0) {
+            adjacents.push(grid[x][y-1]);
+
+            if (x < this.grid.length - 1) {
+                adjacents.push(grid[x+1][y-1]);
+            }
+        }
+
+        if (x < this.grid.length - 1) {
+            adjacents.push(grid[x+1][y]);
+
+            if (y < this.grid[x].length) {
+                adjacents.push(grid[x+1][y+1]);
+            }
+        }
+
+        if (y < this.grid[x].length - 1) {
+            adjacents.push(grid[x][y+1]);
+        }
+
+        return adjacents;
+    }
+}
+
+
+function getGrid() {
+    grid = [];
+    for (var line of input) {
+        grid.push(line.split(''));
+    }
+    return new Grid(grid);
+}
+
+function partOne() {
+    const gridCl = getGrid();
+    const grid = gridCl.get();
+
+    for (var x = 0; x<grid.length; x++) {
+        for (var y=0; y<grid[x].length; y++) {
+            if (grid[x][y] !== paper) continue;
+            const adjacents = gridCl.adjacent(x, y).filter((adj) => adj === paper);
+            if (adjacents.length < maxPaper) {
+                output++;
+            }
+        }
+    }
+
+    console.log("output: ", output)
+}
+
+
+
+partOne();
